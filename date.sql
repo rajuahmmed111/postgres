f9ee76991c474f21b21555e87fa8e984deb99175
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-02 14:11:00 +0600

    update

--- date.sql
+++ date.sql
@@ -21,3 +21,16 @@ SELECT now()::date;
 SELECT now()::time
 
 SELECT to_char(now(), 'yyyy-MM-dd');
+SELECT to_char(now(), 'DDD');
+
+-- interval
+SELECT CURRENT_DATE - INTERVAL '1 year';
+
+SELECT age(CURRENT_DATE, '2003-07-07');
+
+SELECT *, age(CURRENT_DATE, dob) FROM students;
+
+-- extract
+SELECT extract(months FROM '2025-03-02'::date);
+
+SELECT 'n'::BOOLEAN;
