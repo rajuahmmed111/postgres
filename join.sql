b9dd37aea074052a70a240638434687912b0cab0
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-02 19:59:42 +0600

    update

--- join.sql
+++ join.sql
@@ -1,6 +1,20 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
 -- SELECT post.id FROM "post"  JOIN "user" ON post.user_id = "user".id; --provide context(post.id)
 -- SELECT p.id FROM "post" as p JOIN "user" as u ON p.user_id = u.id; --provide context(post.id)
+
+
+-- inner join
 SELECT p.id FROM "post" as p INNER JOIN "user" as u ON p.user_id = u.id; --provide context(post.id)
 
+
+-- LEFT JOIN
+SELECT * FROM "post" as p LEFT JOIN "user" as u ON p.user_id = u.id; -- provide context(post.id)
+
+-- RIGHT JOIN
+
+SELECT * FROM "post" as p RIGHT JOIN "user" as u ON p.user_id = u.id; -- provide context(post.id)
+
 SELECT * FROM post;
+
+INSERT INTO post (id, title, user_id) VALUES
+(6, 'this is a post title', NULL);
