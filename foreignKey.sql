145dbd93c812d916f5cbc869032d968785904361
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-02 16:07:35 +0600

    update

--- foreignKey.sql
+++ foreignKey.sql
@@ -7,7 +7,7 @@ CREATE TABLE "user" (
 CREATE TABLE "post" (
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-    user_id INTEGER REFERENCES "user"(id)
+    user_id INTEGER REFERENCES "user"(id) ON DELETE SET DEFAULT DEFAULT 2
 )
 
 INSERT INTO "user" (username) VALUES 
@@ -30,7 +30,24 @@ ALTER TABLE post
    alter COLUMN user_id set NOT NULL;
 
 
+
+-- deletion
+DELETE FROM "user" WHERE id = 4;
+
+
+SELECT * FROM "user";
+SELECT * FROM "post";
+
+DROP Table "user";
+DROP TABLE post;
+
 ------ insertion constraint on insert post
 -- 1. attempt to insert a new post with the user does not exist
 -- 2. inserting a post with a valid user ID
 -- 3. attempting to insert a post without specifying the user ID
+
+------- deletion constraint on delete user
+-- 1. cascading deletion --> on delete cascade
+-- 2. setting null --> on delete set to null
+-- 3. deletion restrict --> on delete restrict / on delete on action (default)
+-- 4. set default values --> on delete set defaul
