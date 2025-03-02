3d04b50c7fa13e823bc6a804bf066563862b019d
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-02 14:49:03 +0600

    use group and having with aggregate

--- date.sql
+++ date.sql
@@ -18,19 +18,43 @@ SELECT * FROM timeZ;
 SELECT CURRENT_DATE
 
 SELECT now()::date;
+
 SELECT now()::time
 
 SELECT to_char(now(), 'yyyy-MM-dd');
+
 SELECT to_char(now(), 'DDD');
 
 -- interval
 SELECT CURRENT_DATE - INTERVAL '1 year';
 
-SELECT age(CURRENT_DATE, '2003-07-07');
+SELECT age (CURRENT_DATE, '2003-07-07');
 
-SELECT *, age(CURRENT_DATE, dob) FROM students;
+SELECT *, age (CURRENT_DATE, dob) FROM students;
 
 -- extract
-SELECT extract(months FROM '2025-03-02'::date);
+SELECT extract( months FROM '2025-03-02'::date );
 
 SELECT 'n'::BOOLEAN;
+
+-- group clause
+SELECT country , count(*), avg(age) FROM students GROUP BY country;
+
+
+-- 
+
+
+
+---- query using having clause with group clause ----
+
+ -- filter group using having to show only countries with average age above 22
+ SELECT country , count(*), avg(age) FROM students 
+    GROUP BY country
+        HAVING avg(age) > 22;
+
+
+
+ -- count students born in each year
+ SELECT count(*), extract(year FROM dob) as birth_year
+    FROM students
+    GROUP BY birth_year
