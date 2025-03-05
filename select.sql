97bf804c1a91096f2f94b31380f19c8cfd08c82a
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-05 17:10:02 +0600

    aggregate

--- select.sql
+++ select.sql
@@ -443,7 +443,6 @@ SELECT max(length(first_name)) as name FROM students
 -- NOT
 SELECT * FROM students WHERE country <> 'USA';
 
---
 SELECT * FROM students WHERE NOT country = 'USA';
 
 SELECT NULL = 0;
