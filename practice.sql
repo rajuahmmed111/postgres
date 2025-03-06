689921f179d4a841d1bbd2716b64c99337421c55
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-06 15:42:20 +0600

    added the table

--- practice.sql
+++ practice.sql
@@ -1,1 +1,16 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
+
+-- create employee table
+CREATE TABLE employees (
+    e_id SERIAL PRIMARY KEY,
+    e_name VARCHAR(255),
+    d_id INT REFERENCES department (d_id),
+    salary DECIMAL(10, 2),
+    hire_date DATE
+)
+
+-- create department table
+CREATE TABLE department (
+    d_id SERIAL PRIMARY KEY,
+    d_name VARCHAR(255),
+)
