f0913f68756c8e40066a0f4442565e79755bf783
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-06 15:45:30 +0600

    added the table

--- practice.sql
+++ practice.sql
@@ -4,13 +4,13 @@
 CREATE TABLE employees (
     e_id SERIAL PRIMARY KEY,
     e_name VARCHAR(50),
-    d_id INT REFERENCES department (d_id),
+    d_id INT REFERENCES departments (d_id),
     salary DECIMAL(10, 2),
     hire_date DATE
 )
 
 -- create department table
-CREATE TABLE department (
+CREATE TABLE departments (
     d_id SERIAL PRIMARY KEY,
-    d_name VARCHAR(50),
+    d_name VARCHAR(50)
 )
