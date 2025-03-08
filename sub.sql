ca6b71e66abc1084af842b66505f93c92c09bd36
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-08 12:00:12 +0600

    create employee1 table

--- sub.sql
+++ sub.sql
@@ -1,1 +1,9 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
+-- create employee1 table
+CREATE TABLE employees1 (
+    employee_id SERIAL PRIMARY KEY,
+    employee_name VARCHAR(50) NOT NULL,
+    department_name VARCHAR(50),
+    salary DECIMAL(10, 2),
+    hire_date DATE
+);
