f40b0aa21ac53c4e6c93bfd771d6982d2309bd6e
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-08 19:13:39 +0600

    create function

--- functionsProcedure.sql
+++ functionsProcedure.sql
@@ -1,1 +1,13 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
+SELECT * FROM employees1;
+
+-- create function
+CREATE Function emp_count()
+RETURNS INT
+LANGUAGE SQL
+AS
+$$
+SELECT count(*) FROM employees1;
+$$;
+
+SELECT emp_count()
