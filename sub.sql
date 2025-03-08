08d3fc3f05e2b4faf96ca01f4c1d051503c79167
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-08 12:13:39 +0600

    inserted data in emplyee1 table

--- sub.sql
+++ sub.sql
@@ -7,3 +7,10 @@ CREATE TABLE employees1 (
     salary DECIMAL(10, 2),
     hire_date DATE
 );
+
+
+-- insert data into employee1 table
+INSERT INTO employees1 (employee_name, department_name, salary,hire_date) VALUES
+    ('John Doe', 'HR', 60000.00, '2010-01-01'),
+    ('Jane Smith', 'IT', 80000.00, '2015-05-15'),
+    ('Mike Johnson', 'Finance', 70000.00, '2012-07-20');
