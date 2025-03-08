8ac67e7e35e1b6a23f6dc42a8eef9de20ade4435
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-08 15:02:56 +0600

    create view

--- view.sql
+++ view.sql
@@ -4,3 +4,24 @@
 CREATE VIEW dept_avg_salary
 AS
 SELECT department_name, avg(salary) FROM employees1 GROUP BY department_name; 
+
+SELECT * FROM dept_avg_salary;
+
+-- test view
+CREATE View test_view
+AS
+SELECT
+    employee_name,
+    salary,
+    department_name
+FROM employees1
+WHERE
+    department_name in (
+        SELECT department_name
+        FROM employees1
+        WHERE department_name
+            LIKE '%R%'
+    );
+
+
+SELECT * FROM test_view;
