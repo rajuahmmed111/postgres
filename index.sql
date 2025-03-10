57605435a5873223d0ae5ca40d82d755aac19176
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-10 16:39:12 +0600

    add index file

--- index.sql
+++ index.sql
@@ -1,1 +1,15 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
+
+EXPLAIN ANALYSE
+SELECT *
+FROM employees1
+WHERE
+    employee_name = 'John Doe';
+
+SELECT * FROM employees1;
+
+--create indexing
+CREATE INDEX idx_employees1_emp_name ON employees1 (employee_name);
+
+--show data directory
+SHOW data_directory;
