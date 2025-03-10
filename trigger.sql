c38c19ecd3cad7c28c7ba2ad8965dbccfb2e4e66
Raju Ahmmed <ahraju2003@gmail.com>
2025-03-10 15:14:50 +0600

    added the trigger filr

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,8 @@
 -- Active: 1740560784691@@127.0.0.1@5432@ph@public
+
+-- syntax 
+-- CREATE Trigger trigger_name
+-- {BEFORE | AFTER | INSERT OF} {INSERT | DELETE | UPDATE | TRUNCATE}
+-- ON table_name
+-- [for each row]
+-- execute function function_name()
